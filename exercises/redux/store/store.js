012d2b467afe0c0c132b446119257286756697f7
SirMatters <perov.krll@gmail.com>
2020-05-08 13:54:24 +0300

    add TOGGLE_TODO and REMOVE_TODO to reducer

--- exercises/redux/store/store.js
+++ exercises/redux/store/store.js
@@ -1,18 +1,67 @@
-function createStore {
+// library code
+function createStore(reducer) {
   let state;
   let listeners = [];
 
-  const getStore = () => store
+  const getState = () => state;
   const subscribe = (listener) => {
-    listeners.push(listener)
+    listeners.push(listener);
 
     return () => {
-      listeners = listeners.filter(l => l !== listener)
-    }
-  }
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((l) => l());
+  };
 
   return {
-    getStore,
-    subscribe
-  } 
+    getState,
+    subscribe,
+    dispatch,
+  };
 }
+
+// App code
+// reducer function
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.todo];
+    case 'REMOVE_TODO':
+      return state.filter((t) => t.id !== action.todo.id);
+    case 'TOGGLE_TODO':
+      return state.map((t) =>
+        t.id !== action.todo.id ? todo : { ...t, complete: !t.complete }
+      );
+    default:
+      return state;
+  }
+}
+
+const store = createStore(todos);
+store.subscribe(() => {
+  console.log(`The new state is ${JSON.stringify(store.getState())}`);
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: { id: 0, name: 'todo1', complete: false },
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: { id: 1, name: 'todo2', complete: false },
+});
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  todo: { id: 0 },
+});
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  todo: { id: 1 },
+});
