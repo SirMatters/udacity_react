7da2d36e59bf04d8f79edcbc62f3db9c36d684a9
SirMatters <perov.krll@gmail.com>
2020-05-14 20:05:02 +0300

    Replace custom connect(), Provider and context invocation with ReactRedux

--- exercises/redux/store/reacted.js
+++ exercises/redux/store/reacted.js
@@ -1,5 +1,3 @@
-// TODO: add goals remove
-
 const List = (props) => (
   <ul>
     {props.items.map((i) => (
@@ -48,7 +46,7 @@ class Todos extends React.Component {
   }
 }
 
-const ConnectedTodos = connect((state) => ({
+const ConnectedTodos = ReactRedux.connect((state) => ({
   todos: state.todos,
 }))(Todos);
 
@@ -76,57 +74,10 @@ class Goals extends React.Component {
   }
 }
 
-const ConnectedGoals = connect((state) => ({
+const ConnectedGoals = ReactRedux.connect((state) => ({
   goals: state.goals,
 }))(Goals);
 
-class Provider extends React.Component {
-  render() {
-    return (
-      <Context.Provider value={this.props.store}>
-        {this.props.children}
-      </Context.Provider>
-    );
-  }
-}
-
-function connect(mapStateToProps) {
-  return (Component) => {
-    class ConnectedComponent extends React.Component {
-      render() {
-        console.log('in connected comp');
-        return (
-          <Context.Consumer>
-            {(store) => <Receiver store={store} />}
-          </Context.Consumer>
-        );
-      }
-    }
-
-    class Receiver extends React.Component {
-      componentDidMount() {
-        const { subscribe } = this.props.store;
-        this.unsubscribe = subscribe(() => {
-          this.forceUpdate();
-        });
-      }
-
-      componentWillUnmount() {
-        this.unsubscribe();
-      }
-
-      render() {
-        const { dispatch, getState } = this.props.store;
-        const newProps = mapStateToProps(getState());
-        return <Component {...newProps} dispatch={dispatch} />;
-      }
-    }
-    console.log(`In func with ${Component}`);
-    return ConnectedComponent;
-  };
-}
-
-const Context = React.createContext();
 class App extends React.Component {
   componentDidMount() {
     const { dispatch } = this.props;
@@ -147,13 +98,13 @@ class App extends React.Component {
   }
 }
 
-const ConnectedApp = connect((state) => ({
+const ConnectedApp = ReactRedux.connect((state) => ({
   loading: state.loading,
 }))(App);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ReactRedux.Provider store={store}>
     <ConnectedApp />
-  </Provider>,
+  </ReactRedux.Provider>,
   document.getElementById('app')
 );
