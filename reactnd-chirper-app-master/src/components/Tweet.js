fac0bec6316ec97150e146539134cbbc997c5c73
SirMatters <perov.krll@gmail.com>
2020-05-16 15:54:33 +0300

    add parent tweet handling

--- reactnd-chirper-app-master/src/components/Tweet.js
+++ reactnd-chirper-app-master/src/components/Tweet.js
@@ -4,16 +4,22 @@ import { formatTweet } from '../utils/helpers';
 
 class Tweet extends Component {
   render() {
+    if (!tweet) {
+      return <p>Sorry, this tweet does not exist</p>;
+    }
     return <div className='tweet'></div>;
   }
 }
 
 function mapStateToProps({ authedUser, tweets, users }, { id }) {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 }
 
