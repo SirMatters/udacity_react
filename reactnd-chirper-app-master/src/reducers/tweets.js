80333cd23d0a9a77870c1f191a6fa40b41e21064
SirMatters <perov.krll@gmail.com>
2020-05-16 18:16:40 +0300

    Add TOGGLE_TWEET action handling to Tweet reducer

--- reactnd-chirper-app-master/src/reducers/tweets.js
+++ reactnd-chirper-app-master/src/reducers/tweets.js
@@ -3,9 +3,17 @@ import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets';
 const tweets = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_TWEETS:
-      return { ...state.tweets, ...action.tweets };
+      return { ...state, ...action.tweets };
     case TOGGLE_TWEET:
-      return '';
+      return {
+        ...state,
+        [action.tweetId]: {
+          ...state[action.tweetId],
+          likes: action.hasLiked
+            ? state[action.tweetId].likes.filter((u) => u !== action.authedUser)
+            : [...state[action.tweetId].likes, action.authedUser],
+        },
+      };
     default:
       return state;
   }
