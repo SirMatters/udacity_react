98b58a91052af2e846fda569c40828b37f86102e
SirMatters <perov.krll@gmail.com>
2020-05-16 17:15:37 +0300

    add loading bar

--- reactnd-chirper-app-master/src/actions/shared.js
+++ reactnd-chirper-app-master/src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from '../utils/api';
 import recieveUsers from './users';
 import recieveTweets from './tweets';
 import setAuthedUser from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'dan_abramov';
 
 const handleInitialData = () => {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, tweets }) => {
       dispatch(recieveTweets(tweets));
       dispatch(recieveUsers(users));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 };
--- reactnd-chirper-app-master/src/components/App.js
+++ reactnd-chirper-app-master/src/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import handleInitialData from '../actions/shared';
 import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>{this.props.authedUser && <Dashboard />}</div>;
+    return <div>{this.props.authedUser ? <Dashboard /> : <LoadingBar />}</div>;
   }
 }
 
