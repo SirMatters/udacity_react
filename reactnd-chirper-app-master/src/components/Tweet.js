760513b12dc7b4dde232e20a8ecdedd06dab6007
SirMatters <perov.krll@gmail.com>
2020-05-16 16:22:11 +0300

    finalize Tweet component

--- reactnd-chirper-app-master/src/components/Tweet.js
+++ reactnd-chirper-app-master/src/components/Tweet.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { formatTweet } from '../utils/helpers';
+import { formatTweet, formatDate } from '../utils/helpers';
+import {
+  TiArrowBackOutline,
+  TiHeartOutline,
+  TiHeartFullOutline,
+} from 'react-icons/ti/index';
 
 class Tweet extends Component {
+  handleLike = (e) => {
+    e.preventDefault();
+  };
+
+  toParent = (e, id) => {
+    e.preventDefault();
+    // TODO: redirect to tweet
+  };
+
   render() {
+    const { tweet } = this.props;
     if (!tweet) {
       return <p>Sorry, this tweet does not exist</p>;
     }
-    return <div className='tweet'></div>;
+
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      hasLiked,
+      likes,
+      replies,
+      parent,
+    } = tweet;
+
+    return (
+      <div className='tweet'>
+        <img src={avatar} alt={`Avatar of ${name}`} className='avatar' />
+        <div className='tweet-info'>
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className='replying-to' onClick={(e) => this.toParent()}>
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className='tweet-icons'>
+            <TiArrowBackOutline className='tweet-icon' />
+            <span>{replies !== 0 && replies}</span>
+            <button className='heart-button' onClick={this.handleLike}>
+              {hasLiked ? (
+                <TiHeartFullOutline color='#e0425e' className='tweet-icon' />
+              ) : (
+                <TiHeartOutline className='tweet-icon' />
+              )}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
